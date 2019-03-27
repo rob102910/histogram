0869a19cca17c3ee6b2520b1711aa3d93c69f3f6
WynnieSun <wynniesun@WynnieSundeMacBook-Air.local>
2019-03-26 21:21:23 -0400

    c

--- histogram.js
+++ histogram.js
@@ -2,7 +2,7 @@ var datahistogram = d3.json("data.json");
 
 datahistogram.then(function(data)
 {
-  drawGraph(data,2);
+  drawGraph(data,1);
 },
 function(err)
 {
@@ -31,30 +31,32 @@ right:10
 var width = screen.width - margins.left - margins.right;
 var height = screen.height - margins.top - margins.bottom;
 
-var xScale = d3.scalelinear()
-            .domain(d3.extent(data))
+var xScale = d3.scaleLinear()
+            .domain([0,10])
             .nice()
             .range([0.,width]);
 
 var binMaker = d3.histogram()
             .domain(xScale.domain())
-            .threshold(xScale.ticks(50)); //might need to change ticks
+            .thresholds(xScale.ticks(5)); //might need to change ticks
 
 var newArray = data.map(function(d)
 {
-  return d[1][(day/2)-1];
+  return d.quizes[day-1].grade;
 })
+console.log(newArray)
+
 var bins = binMaker(newArray)
+window.alert(bins)
+//var percentage = function(d)
+//{
+//  return d.length/data.length
+//}
 
-var percentage = function(d)
-{
-  return d.length/data.length
-}
-
-var yScale = d3.scalelinear()
-            .domain([0,d3.max(bins,function(d){return percentage(d);})])
-              .range([height,0])
-              .nice();
+var yScale = d3.scaleLinear()
+               .domain([0,23])
+               .range([height,0])
+               .nice();
 
 var colors = d3.scaleOrdinal(d3.schemeSet3);
 
@@ -65,10 +67,10 @@ plot.selectAll("rect")
     .data(bins)
     .enter()
     .append("rect")
-    .attr("x",function(d,i) {return xScale(i);})
-    .attr("width",xScale(width/data.length))
-    .attr("y",function(d) {return yScale(percentage(d));})
-    .attr("height",function(d)  {return height- yScale(percentage(d));})
+    .attr("x",function(d) {return xScale(d.x0);})
+    .attr("width",function(d){return xScale(d.x1-.1) - xScale(d.x0);})
+    .attr("y",function(d) {return yScale(d.length)})
+    .attr("height",function(d) {return height- yScale(d.length)})
     //.attr("fill",function(d){return colors(d.homework);})
 
 var xAxis = d3.axisBottom()
