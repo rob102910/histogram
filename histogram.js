c3b9d573c48db5bcdd9a9ed5c942025e1e7be026
WynnieSun <wynniesun@WynnieSundeMacBook-Air.local>
2019-03-29 16:33:52 -0400

    d

--- histogram.js
+++ histogram.js
@@ -5,10 +5,27 @@ var screen =
  width:500,
  height:500
 };
+var margins =
+{
+top: 30,
+bottom: 30,
+left: 30,
+right:30
+};
 
+var bars = 11;
 
-var changeGraph = function(data){
+var width = screen.width - margins.left - margins.right;
+var height = screen.height - margins.top - margins.bottom;
+
+var xScale = d3.scaleLinear()
+            .domain([0,11])
+            .nice()
+            .range([0.,width]);
+
+var colors = d3.scaleOrdinal(d3.schemeYlGnBu[0,9]);
 // display days and change graph
+var changeGraph = function(data){
 var svg = d3.select("#day")
           .attr("width",screen.width+1000)
           .attr("height",screen.height-450);
@@ -36,14 +53,14 @@ var newplot = d3.select("svg")
         d3.select(this).attr("fill","black")
           .attr("font-size","16px")
       })
-      .on("click",function(d){
+      .on("click",function(d,i){
         datahistogram.then(function(data){
-        drawGraph(data,d.day)}
+        update(data,i)} //day (i+1) - index i
       )})
 
 }
 
-//draw graph
+//draw initial graph day 1
 var drawGraph = function(data,day)
 {
 
@@ -51,65 +68,38 @@ var svg = d3.select("#graph")
             .attr("width",screen.width+1000)
             .attr("height",screen.height+1000);
 
-var margins =
-{
-top: 30,
-bottom: 30,
-left: 30,
-right:30
-};
-
-var bars = 11;
-
-var width = screen.width - margins.left - margins.right;
-var height = screen.height - margins.top - margins.bottom;
-
-var xScale = d3.scaleLinear()
-            .domain([0,11])
-            .nice()
-            .range([0.,width]);
-
 var binMaker = d3.histogram()
             .domain(xScale.domain())
             .thresholds(xScale.ticks(bars)); //might need to change ticks
 
 var newArray = data.map(function(d)
 {
-  return d.quizes[day-1].grade;
+  return d.quizes[day].grade;
 })
 //console.log(newArray)
-
 var bins = binMaker(newArray)
 //console.log(bins)
-
 var max = d3.max(bins,function(d)
 {
   return d.length
 })
-
 //console.log(max)
-
 //window.alert(bins)
-//var percentage = function(d)
-//{
-//  return d.length/data.length
-//}
-
 var yScale = d3.scaleLinear()
                .domain([0,max])
                .range([height,0])
                .nice();
 
-var colors = d3.scaleOrdinal(d3.schemeYlGnBu[0,9]);
-
 var plot = svg.append("g")
-              .attr("transform","translate("+margins.left+","+margins.top+ ")");
-
+              .attr("transform","translate("+margins.left+","+margins.top+ ")")
+              .attr("id","initial");
 //rects
 plot.selectAll("rect")
     .data(bins)
     .enter()
     .append("rect")
+  //  .select("#initial").selectAll("rect")
+  //  .data(bins)
     .attr("x",function(d) {return xScale(d.x0)+50;})
     .attr("width",width/bars)
     .attr("y",function(d) {return yScale(d.length)+100})
@@ -121,7 +111,6 @@ plot.selectAll("rect")
     					//Get this bar's x/y values, then augment for the tooltip
     					var xPosition = parseFloat(d3.select(this).attr("x")) + width/bars / 2;
     					var yPosition = parseFloat(d3.select(this).attr("y")) / 2 + height / 2;
-
     					//Update the tooltip position and value
     					d3.select("#tooltip")
     						.style("left", xPosition + "px")
@@ -133,18 +122,13 @@ plot.selectAll("rect")
                 .style("top", yPosition + "px")
                 .select("#grade")
                 .text(d[0])
-
     					//Show the tooltip
     					d3.select("#tooltip").classed("hidden", false);
-
     			   })
-    .on("mouseout", function() {
-
+                .on("mouseout", function() {
     					//Hide the tooltip
     					d3.select("#tooltip").classed("hidden", true);
-
             })
-
 
 var xAxis = d3.axisBottom()
               .scale(xScale)
@@ -154,16 +138,71 @@ var yAxis = d3.axisLeft()
               .scale(yScale)
               .ticks(max);
 
-svg.append("g")
+d3.select("#graph").append("g")
    .attr("id", "xAxis")
    .call(xAxis)
    .attr("transform","translate("+(margins.left+50)+","+(height+100+margins.top)+")");
-svg.append("g")
+d3.select("#graph").append("g")
+   .attr("id","yAxis")
    .call(yAxis)
    .attr("transform","translate("+(margins.left+50)+","+(100+margins.top)+")");
 
 }
 
+//update graph function
+var update = function(data,day){
+  var binMaker = d3.histogram()
+              .domain(xScale.domain())
+              .thresholds(xScale.ticks(bars)); //might need to change ticks
+
+  var newArray = data.map(function(d)
+  {
+    return d.quizes[day].grade;
+  })
+  //console.log(newArray)
+
+  var bins = binMaker(newArray)
+  //console.log(bins)
+
+  var max = d3.max(bins,function(d)
+  {
+    return d.length
+  })
+  var yScale = d3.scaleLinear()
+                 .domain([0,max])
+                 .range([height,0])
+                 .nice();
+  d3.select("#initial")
+    .selectAll("rect")
+    .data(bins)
+    .transition()
+    .attr("x",function(d) {return xScale(d.x0)+50;})
+    .attr("width",width/bars)
+    .attr("y",function(d) {return yScale(d.length)+100})
+    .attr("height",function(d) {return height- yScale(d.length)})
+
+    var xAxis = d3.axisBottom()
+                  .scale(xScale)
+                  .ticks(10);
+
+    var yAxis = d3.axisLeft()
+                  .scale(yScale)
+                  .ticks(max);
+
+    d3.select("#xAxis") //select the old axis and then make change
+       .attr("id", "xAxis")
+       .call(xAxis)
+       .attr("transform","translate("+(margins.left+50)+","+(height+100+margins.top)+")");
+    d3.select("#yAxis")
+       .call(yAxis)
+       .attr("transform","translate("+(margins.left+50)+","+(100+margins.top)+")");
+}
+
+//call the initial function to draw day 1
+datahistogram.then(function(data,day){
+  drawGraph(data,0);
+});
+//call the timeline function to draw days and trigger updata
 datahistogram.then(function(data)
 {
   changeGraph(data);
